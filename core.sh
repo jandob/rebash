6ebb05d7c4221fce06dd11d564eb550d5a4dca23
jandob <janosch.dobler@gmail.com>
2015-12-03 14:35:23 +0100

    fix

--- core.sh
+++ core.sh
@@ -10,20 +10,35 @@ core_sourcer_filename=$(basename "${BASH_SOURCE[1]}")
 core_sourcer_module_name="${core_sourcer_filename%.*}"
 core_imported_modules=($core_sourcer_module_name)
 core_import() {
+    echo importing $1
     local module="$1"
+    local module_path=""
+    local path="$(dirname ${BASH_SOURCE[0]})"
+    # try absolute
+    if [[ $module == /* ]] && [ -e "$module" ];then
+        module_path="$module"
+        module=$(basename "$module_path")
+    fi
+    # todo try relative
+    # try rebash modules
+    if [ -e "$path"/"$module".sh ]; then
+        module_path="$path"/"$module".sh
+    fi
+
+    if [ "$module_path" = "" ]; then
+        return 1
+    fi
+
     # check if module already loaded
     local loaded_module
     for loaded_module in ${core_imported_modules[@]}; do
         [[ "$loaded_module" == "$module" ]] && return 0
     done
+
     core_check_namespace $module
     core_imported_modules+=("$module")
-    local path="$(dirname ${BASH_SOURCE[0]})"
-    if [ -e "$path"/"$module"];then
-        source "$path"/${module}
-    else
-        source "$path"/${module}.sh
-    fi
+
+    source "$module_path"
 }
 core_check_namespace() {
     local namespace="$1"

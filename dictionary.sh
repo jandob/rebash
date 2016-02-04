fa2f5a417d5d1f0e3692041c746907a0ffc3aacc
jandob <janosch.dobler@gmail.com>
2016-02-04 14:46:58 +0100

    add support for multiple arguments to dictionary_set

--- dictionary.sh
+++ dictionary.sh
@@ -13,9 +13,11 @@ dictionary_set() {
     >>> dictionary_set map foo 2
     >>> echo ${dictionary__store_map[foo]}
     2
-    >>> dictionary_set map foo "a b c"
+    >>> dictionary_set map foo "a b c" bar 5
     >>> echo ${dictionary__store_map[foo]}
+    >>> echo ${dictionary__store_map[bar]}
     a b c
+    5
 
     >>> dictionary__bash_version_test=true
     >>> dictionary_set map foo 2
@@ -27,15 +29,19 @@ dictionary_set() {
     a b c
     '
     local name="$1"
-    local key="$2"
-    local value="\"$3\""
-    if (($BASH_VERSINFO < 4)) \
-            || ! [ -z "$dictionary__bash_version_test" ]; then
-        eval "dictionary__store_${name}_${key}=""$value"
-    else
-        declare -Ag "dictionary__store_${name}"
-        eval "dictionary__store_${name}[${key}]=""$value"
-    fi
+    while true; do
+        local key="$2"
+        local value="\"$3\""
+        shift 2
+        if (($BASH_VERSINFO < 4)) \
+                || ! [ -z "$dictionary__bash_version_test" ]; then
+            eval "dictionary__store_${name}_${key}=""$value"
+        else
+            declare -Ag "dictionary__store_${name}"
+            eval "dictionary__store_${name}[${key}]=""$value"
+        fi
+        (( $# == 1 )) && return
+    done
 }
 dictionary_get() {
     local __doc__='
@@ -44,19 +50,29 @@ dictionary_get() {
 
     Examples:
 
+    >>> dictionary_get unset_map unset_value
+    >>> dictionary_get unset_map unset_value; echo $?
+    1
+    >>> dictionary__bash_version_test=true
+    >>> dictionary_get unset_map unset_value; echo $?
+    1
+
     >>> dictionary_set map foo 2
     >>> dictionary_set map bar 1
     >>> dictionary_get map foo
     >>> dictionary_get map bar
     2
     1
+
     >>> dictionary_set map foo "a b c"
     >>> dictionary_get map foo
     a b c
+
     >>> dictionary__bash_version_test=true
     >>> dictionary_set map foo 2
     >>> dictionary_get map foo
     2
+
     >>> dictionary__bash_version_test=true
     >>> dictionary_set map foo "a b c"
     >>> dictionary_get map foo
@@ -70,7 +86,9 @@ dictionary_get() {
     else
         local store="dictionary__store_${name}[${key}]"
     fi
-    echo "${!store}"
+    core_is_defined $store || return 1
+    local value="${!store}"
+    echo "$value"
 }
 alias dictionary.set='dictionary_set'
 alias dictionary.get='dictionary_get'

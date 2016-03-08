20398009e4b69259be119df26a6eb47710f2fed7
jandob <janosch.dobler@gmail.com>
2016-03-08 19:57:38 +0100

    add path functions

--- core.sh
+++ core.sh
@@ -22,6 +22,71 @@ core_abs_path() {
         echo "${abs_path_dir}/${file_name}"
     fi
 }
+core_rel_path() {
+    local __doc__='
+    Stolen from http://stackoverflow.com/a/12498485/31038
+    >>> core_rel_path "/A/B/C" "/A"
+    ../..
+    >>> core_rel_path "/A/B/C" "/A/B"
+    ..
+    >>> core_rel_path "/A/B/C" "/A/B/C"
+
+    >>> core_rel_path "/A/B/C" "/A/B/C/D"
+    D
+    >>> core_rel_path "/A/B/C" "/A/B/C/D/E"
+    D/E
+    >>> core_rel_path "/A/B/C" "/A/B/D"
+    ../D
+    >>> core_rel_path "/A/B/C" "/A/B/D/E"
+    ../D/E
+    >>> core_rel_path "/A/B/C" "/A/D"
+    ../../D
+    >>> core_rel_path "/A/B/C" "/A/D/E"
+    ../../D/E
+    >>> core_rel_path "/A/B/C" "/D/E/F"
+    ../../../D/E/F
+    '
+    # both $1 and $2 are absolute paths beginning with /
+    # returns relative path to $2/$target from $1/$source
+    source="$1"
+    target="$2"
+    [[ -z "$source" ]] && return 1
+    [[ -z "$target" ]] && return 1
+
+    common_part=$source # for now
+    result="" # for now
+
+    while [[ "${target#$common_part}" == "${target}" ]]; do
+        # no match, means that candidate common part is not correct
+        # go up one level (reduce common part)
+        common_part="$(dirname "$common_part")"
+        # and record that we went back, with correct / handling
+        if [[ -z $result ]]; then
+            result=".."
+        else
+            result="../$result"
+        fi
+    done
+
+    if [[ $common_part == "/" ]]; then
+        # special case for root (no common path)
+        result="$result/"
+    fi
+
+    # since we now have identified the common part,
+    # compute the non-common part
+    forward_part="${target#$common_part}"
+
+    # and now stick all parts together
+    if [[ -n $result ]] && [[ -n $forward_part ]]; then
+        result="$result$forward_part"
+    elif [[ -n $forward_part ]]; then
+        # extra slash removal
+        result="${forward_part:1}"
+    fi
+
+    echo "$result"
+}
 
 core_imported_modules=("$(core_abs_path "${BASH_SOURCE[0]}")")
 core_imported_modules+=("$(core_abs_path "${BASH_SOURCE[1]}")")
@@ -162,7 +227,7 @@ core_import() {
     local caller_path
     caller_path="$(core_abs_path "$(dirname "${BASH_SOURCE[1]}")")"
     # try absolute
-    if [[ $module == /* ]] && [ -e "$module" ];then
+    if [[ $module == /* ]] && [[ -e "$module" ]];then
         module_path="$module"
         module=$(basename "$module_path")
     fi
@@ -172,7 +237,7 @@ core_import() {
         module=$(basename "$module_path")
     fi
     # try rebash modules
-    if [ -e "$path"/"$module".sh ]; then
+    if [[ -e "$path"/"$module".sh ]]; then
         module_path="$path"/"$module".sh
     fi
 
@@ -188,8 +253,7 @@ core_import() {
 
     core_imported_modules+=("$module_path")
     core_source_with_namespace_check "$module_path" "${module%.sh}"
-    #core_check_namespace ${module%.*}
-
-    #source "$module_path"
 }
 alias core.import="core_import"
+alias core.abs_path="core_abs_path"
+alias core.rel_path="core_rel_path"
